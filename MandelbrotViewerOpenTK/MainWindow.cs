acce61754afa8d94a86158407b96fed33a1c1e23
Cwazywierdo <joshbothhoth@gmail.com>
2022-02-14 20:05:06 -0600

    Create and implement MainWindow class
    - Create MainWindow class with basic update and draw overrides
    - Create and run new MainWindow with initialized window settings

---
+++ MandelbrotViewerOpenTK/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MandelbrotViewerOpenTK
+{
+    class MainWindow : GameWindow
+    {
+        public MainWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+        }        
+    }
+}
