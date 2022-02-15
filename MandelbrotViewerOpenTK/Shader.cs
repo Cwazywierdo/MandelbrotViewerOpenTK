c00a0243304b3edd66f3cf87e4e776bf7a2c3d25
Cwazywierdo <joshbothhoth@gmail.com>
2022-02-14 21:29:13 -0600

    Create Shader.cs
    Create the Shader class that handles compilation, usage, and destruction of glsl shaders.

---
+++ MandelbrotViewerOpenTK/Shader.cs
@@ -0,0 +1,70 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.IO;
+using System.Text;
+
+namespace MandelbrotViewerOpenTK
+{
+    class Shader : IDisposable
+    {
+        readonly int Handle;
+        private bool disposedValue = false;
+
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            Handle = GL.CreateProgram();
+
+            int vertexShader = ProcessShader(vertexPath, ShaderType.VertexShader);
+            int fragmentShader = ProcessShader(fragmentPath, ShaderType.FragmentShader);
+
+            GL.LinkProgram(Handle);
+
+            GL.DetachShader(Handle, vertexShader);
+            GL.DeleteShader(vertexShader);
+
+            GL.DetachShader(Handle, fragmentShader);
+            GL.DeleteShader(fragmentShader);
+        }
+        private int ProcessShader(string path, ShaderType shaderType)
+        {
+            string shaderSource;
+            using (StreamReader reader = new StreamReader(path, Encoding.UTF8))
+            {
+                shaderSource = reader.ReadToEnd();
+            }
+
+            int shader = GL.CreateShader(shaderType);
+
+            GL.ShaderSource(shader, shaderSource);
+            GL.CompileShader(shader);
+
+            string infoLogVert = GL.GetShaderInfoLog(shader);
+            if (infoLogVert != string.Empty)
+                Console.WriteLine(infoLogVert);
+
+            GL.AttachShader(Handle, shader);
+
+            return shader;
+        }
+
+        ~Shader() => GL.DeleteProgram(Handle);
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                GL.DeleteProgram(Handle);
+                disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Use() => GL.UseProgram(Handle);
+
+        
+
+    }
+}
