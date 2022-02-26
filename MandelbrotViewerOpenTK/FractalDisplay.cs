42808681e378ec5496fb0e0c2e1a882989fb416f
Cwazywierdo <joshbothhoth@gmail.com>
2022-02-25 18:40:16 -0600

    Add FractalDisplay.IteratePoint method

--- MandelbrotViewerOpenTK/FractalDisplay.cs
+++ MandelbrotViewerOpenTK/FractalDisplay.cs
@@ -66,5 +66,12 @@ namespace MandelbrotViewerOpenTK
 
             GL.DrawArrays(PrimitiveType.TriangleFan, 0, 4);
         }
+
+        public void IteratePoint(ref Vector2d z, Vector2d c)
+        {
+            double tmpX = z.X;
+            z.X = z.X * z.X - z.Y * z.Y + c.X;
+            z.Y = 2 * tmpX * z.Y + c.Y;
+        }
     }
 }
